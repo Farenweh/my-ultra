d78e4b49eb50b3a18204d4751640207cfa9ec405
Chen Buyi <w1241385936@outlook.com>
2025-04-12 21:19:19 +0800

    上游更新前提交

--- ultralytics/__init__.py
+++ ultralytics/__init__.py
@@ -8,13 +8,10 @@ import os
 if not os.environ.get("OMP_NUM_THREADS"):
     os.environ["OMP_NUM_THREADS"] = "1"  # default for reduced CPU utilization during training
 
-# 用于抑制多进程移除临时文件，避免无关紧要的错误报告。这可能需要py3.8及以上
-os.environ["PYTHONMULTIPROCESSINGCLEANUP"] = "none"
-
 import comet_ml  # 为了获取全部日志
 import torch
 
-torch.set_num_threads(32)  # 减少进程数避免CPU占用过高
+torch.set_num_threads(16)  # 减少进程数避免CPU占用过高
 
 from ultralytics.models import NAS, RTDETR, SAM, YOLO, YOLOE, FastSAM, YOLOWorld
 from ultralytics.utils import ASSETS, SETTINGS
