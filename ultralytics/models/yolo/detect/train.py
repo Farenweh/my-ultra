d59cd117870737474ebe8f121bc0bfa8ffa86d7b
Chen Buyi <w1241385936@outlook.com>
2025-04-13 22:20:15 +0800

    修复了训练时的ddp val，但是手动初始化的仍然有问题

--- ultralytics/models/yolo/detect/train.py
+++ ultralytics/models/yolo/detect/train.py
@@ -62,9 +62,6 @@ class DetectionTrainer(BaseTrainer):
         Returns:
             (Dataset): YOLO dataset object configured for the specified mode.
         """
-<<<<<<< HEAD
-        gs = int(de_parallel(self.model).stride.max() if self.model else 0)
-=======
         gs = max(int(de_parallel(self.model).stride.max() if self.model else 0), 32)
 >>>>>>> 9a1314c5 (修复了训练时的ddp val，但是手动初始化的仍然有问题)
         return build_yolo_dataset(self.args, img_path, batch, self.data, mode=mode, rect=rect, stride=gs)
