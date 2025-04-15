89f86167393707430d129ff456563dfaccc997c3
Chen Buyi <w1241385936@outlook.com>
2025-04-16 01:10:18 +0800

    稳定版本，除了手动val ddp之外应该全部稳定

--- dev.py
+++ dev.py
@@ -1,11 +1,21 @@
 from ultralytics import YOLO
 
-# Load a model
-model = YOLO("yolo11n.pt")
+model = YOLO("dinov2-yolo11.yaml")
+# model = YOLO("yolo11n.yaml")
 
 model.train(
     data="coco.yaml",
-    batch=128 * 2,
-    device=[3, 4],
+    imgsz=644,
+    optimizer="AdamW",
+    lr0=1e-3,
+    momentum=0.9,
+    batch=128,
+    device=[7],
+    freeze=[0],
     epochs=1,
+    close_mosaic=999,
+    project="dinov2-yolo11",
+    name="l-coco-devtest",
+    exist_ok=True,
+    fraction=2e-3,
 )
